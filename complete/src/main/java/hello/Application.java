18d5e831739acf87aafc064c5e25646848f5671e
Robert Outlaw <routlaw@microsoft.com>
2017-08-08 11:45:07 -0700

    Update Application.java

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World on Azure. Have a donut!";
+        return "Hello Docker World on Azure";
     }
 
     public static void main(String[] args) {
